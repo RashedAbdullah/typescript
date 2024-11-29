f6f9bdc6fcd67169453df4f59c8d0acce0f734df
RashedAbdullah <maarashed40@gmail.com>
2024-11-29 19:15:06 +0600

    TypeScript learning

--- basics/dist/index.js
+++ basics/dist/index.js
@@ -1,8 +1,8 @@
 "use strict";
-
 let num2 = 45;
 num2 = 503487;
 function anyFunc(a, b) {
-  return a + b;
+    return a + b;
 }
 anyFunc(3, 4);
+//# sourceMappingURL=index.js.map
--- basics/ts/index.ts
+++ basics/ts/index.ts
@@ -1,14 +1,37 @@
-let num2: number = 45;
+// let num2: number = 45;
 
-num2 = 503487;
+// num2 = 503487;
 
-function anyFunc(a: number, b: number): number {
-  return a + b;
-}
+// function anyFunc(a: number, b: number): number {
+//   return a + b;
+// }
 
-anyFunc(3, 4);
+// anyFunc(3, 4);
 
-type User = {
-  name: string;
-  age: number;
+// type User = {
+//   name: string;
+//   age: number;
+// };
+
+// const users: object = [
+//   {
+//     name: "Rashed Abdullah",
+//     age: 25,
+//   },
+// ];
+
+let bums: number = 453;
+// const bigNums: bigint = 4098574305984058974985n;
+
+let arr: any[] = [4545];
+let arr2: (string | number | boolean)[] = [54, "sdf", false];
+let arr3: [number, string, boolean] = [54, "sdf", false];
+
+let obj: { name: string; age: number } = {
+  name: "Rashed",
+  age: 25,
 };
+
+enum Users {
+  Name = 1,
+}
